ac86d0b2ff1f748447fd2cb27439e83aa7073930
Achmad Fatoni <achmadfatony@gmail.com>
2016-08-01 02:06:07 +0800

    turn back site protect

--- src/Klsandbox/OrientationRoute/OrientationRouteServiceProvider.php
+++ src/Klsandbox/OrientationRoute/OrientationRouteServiceProvider.php
@@ -4,6 +4,8 @@ namespace Klsandbox\OrientationRoute;
 
 use Illuminate\Routing\Router;
 use Illuminate\Support\ServiceProvider;
+use Klsandbox\OrientationRoute\Models\Video;
+use Klsandbox\SiteModel\Site;
 
 class OrientationRouteServiceProvider extends ServiceProvider
 {
@@ -51,8 +53,8 @@ class OrientationRouteServiceProvider extends ServiceProvider
         ], 'migrations');
 
         $router->bind('video', function ($id) {
-            $video = \Klsandbox\OrientationRoute\Models\Video::find($id);
-
+            $video = Video::find($id);
+            Site::protect($video, 'Video');
             return $video;
         });
     }
