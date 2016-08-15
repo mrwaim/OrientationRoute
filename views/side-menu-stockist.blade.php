af64ca8dd77594745dadbd51c22746cb9a2e0c8a
Achmad Fatoni <achmadfatony@gmail.com>
2016-08-15 11:02:58 +0800

    put back videos side menu

--- views/side-menu-stockist.blade.php
+++ views/side-menu-stockist.blade.php
@@ -1,11 +1,21 @@
+<?php
+$videoSubmenu = [];
+foreach ($videos as $video) {
+   $videoSubmenu[] = [
+        'videoFilter' => $video->id,
+        'url' => $video->id . '/view',
+        'menu' => ($video->watchedByUser($auth->user) ? '✔' : '') . ' ' . $video->title,
+    ];
+}
+?>
 @include('elements.side-menu-parent-item', [
 'folder' => 'videos',
 'menu' => 'Orientation Video',
 'menuIcon' => 'fa-graduation-cap',
-'children' =>
- [ [
+'children' => array_merge($videoSubmenu, [ [
         'page' => 'all',
         'url' => 'all',
         'menu' => 'View All Videos',
     ]
-]])
+])
+])
